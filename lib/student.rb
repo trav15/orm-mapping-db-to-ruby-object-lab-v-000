812643df94dabb38af20a5a0219eae9ea3b9e613
Travis T <travistokuyama@gmail.com>
2018-11-02 19:08:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ class Student
       WHERE grade = 9
     SQL
 
-    DB[:conn].execute(sql, grade).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
